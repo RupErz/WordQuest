57657b90d3d75782fdc2bd7d7c43b25f65f50978
Nghia Vu <nghiavu@Nghias-MacBook-Pro.local>
2025-10-04 13:36:24 -0400

    First commit

---
+++ WordQuest/WordQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordQuestApp.swift
+//  WordQuest
+//
+//  Created by Nghia Vu on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WordQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordQuestTests/WordQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordQuestTests.swift
+//  WordQuestTests
+//
+//  Created by Nghia Vu on 10/4/25.
+//
+
+import Testing
+@testable import WordQuest
+
+struct WordQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
